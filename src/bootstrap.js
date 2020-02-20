f4fbd407a0f677289dceaae0ad945f0bfa99f683
Evan McPheron <evan.mcpheron@icloud.com>
2020-02-20 08:02:07 -0800

    added styling to header/nav/body

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,25 +1,33 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter } from "react-router-dom";
-import reducers from "./reducers";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { Router, Switch, Route } from 'react-router-dom';
+import reducers from './reducers';
 
-import Layout from "./components/layout";
+import Layout from './components/layout';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-import "./style/import.scss";
+import './style/import.scss';
+
+import history from './history';
+import SignIn from './components/auth/signin';
+import SignUp from './components/auth/signup';
 
 function main() {
-  ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
-      <BrowserRouter>
-        <Layout></Layout>
-      </BrowserRouter>
-    </Provider>,
-    document.querySelector(".app-wrapper")
-  );
+    ReactDOM.render(
+        <Provider store={createStoreWithMiddleware(reducers)}>
+            <Router history={history}>
+                <Layout>
+                    <Switch>
+                        <Route path="/" exact component={SignIn}/>
+                    </Switch>
+                </Layout>
+            </Router>
+        </Provider>,
+        document.querySelector('.app-wrapper')
+    );
 }
 
-document.addEventListener("DOMContentLoaded", main);
+document.addEventListener('DOMContentLoaded', main);
